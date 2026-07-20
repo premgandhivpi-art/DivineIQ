6123aab6daefa6a3883729e271cc5e11b4ea008b
premgandhivpi-art <premgandhivpi@gmail.com>
2026-07-20 18:04:36 +0530

    Create MainActivity.kt

---
+++ app/src/main/java/com/divineiq/app/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.divineiq.app
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        
+        webView = WebView(this)
+        setContentView(webView)
+
+        webView.settings.javaScriptEnabled = true
+        webView.settings.domStorageEnabled = true
+        webView.webViewClient = WebViewClient()
+
+        // अपनी वेबसाइट या नोट्स का URL यहाँ लोड करें
+        webView.loadUrl("https://google.com")
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+}
